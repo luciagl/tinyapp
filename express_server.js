9dcb5ec49f8ef9bd73a10b75b7258b8da3a7d825
Lucia Glutnik <glutnik.lucia@gmail.com>
2021-08-02 19:34:11 -0700

    URL Submission Form

--- express_server.js
+++ express_server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = function() {
+  return 'xxxxxx'.replace(/[x]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,7 +40,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  console.log(generateRandomString());
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
